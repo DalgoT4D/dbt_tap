30f6c904075de871e0c4bdc84f4916397c5d093c
Ishan <koradiaishan335@gmail.com>
2023-09-13 11:56:41 +0530

    student access rate and submission rate - WIP - didn't get any match , need to upload more data on frappe

---
+++ models/intermediate/metrics/student_metrics.sql
@@ -0,0 +1,12 @@
+{{ config(materialized="table", schema="prod") }}
+
+select *
+from {{ ref("crm_students_merged") }} as students
+inner join
+    {{ ref("glific_messages_deserialize") }} as messages
+    on concat('91', students.phone) = messages.contact_phone
+where
+    messages.activity_status is not null
+    and students.course_id = messages.course_id
+    and students.batch_id = messages.batch_id
+    
