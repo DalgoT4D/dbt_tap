cfe6d86907fae82ca165f1a76866f7d0489adbab
Ishan <koradiaishan335@gmail.com>
2023-10-12 16:30:10 +0530

    shifting to tap gcp instance

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,10 +1,11 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-
+{% macro generate_schema_name(
+        custom_schema_name,
+        node
+    ) -%}
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%} {{ default_schema }}
-
-    {%- else -%} {{ "tap" }}_{{ custom_schema_name }}
-
+    {%- if custom_schema_name is none -%}
+        {{ default_schema }}
+    {%- else -%}
+        {{ "dalgo" }}_{{ custom_schema_name }}
     {%- endif -%}
-
 {%- endmacro %}
